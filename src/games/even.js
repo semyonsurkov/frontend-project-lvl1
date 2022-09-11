f0b59efc7102afb8f45768419f71238c6ca23e2d
semyonsurkov <semyonsurkov@outlook.com>
2022-09-11 10:29:39 +0300

    update even.js

--- src/games/even.js
+++ src/games/even.js
@@ -6,7 +6,6 @@ const description = 'Answer "yes" if the number is even, otherwise answer "no".'
 const runEven = () => {
 	const tasks = [];
 	let i = 0;
-	/// ... решение
 	while (i !== 3) {
 		const question = getRandomNumber(1, 10);
 		const rightAnswer = (question % 2 === 0 ? 'yes' : 'no');
@@ -15,6 +14,5 @@ const runEven = () => {
 	}
 	run(tasks, description);
 };
-// вызов функции, которая соберет список массивов [[вопрос1, ответ1], [вопрос2, ответ2], [вопрос3, ответ3]]
-// не усложнять именование переменных, например startTheGame = run, не увеличивать запись
+
 export default runEven; 
