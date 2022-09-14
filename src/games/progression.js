7a86f355a1265ba27a64d53e21c37d0143272c4c
semyonsurkov <semyonsurkov@outlook.com>
2022-09-14 11:08:02 +0300

    update the game logic progression.js

--- src/games/progression.js
+++ src/games/progression.js
@@ -4,22 +4,27 @@ import getRandomNumber from '../utils.js';
 const description = 'What number is missing in the progression?';
 
 const getProgression = (start, step, length) => {
-  const progressionItems = [];
-  for (let i = start; i < length; i += step) {
-    progressionItems.push(i);
+	const progressionItems = [];
+  let element = start;
+  for (let i = 1; i < length; i += 1) {
+	  progressionItems.push(element);
+	  element += step;
   }
-  return [progressionItems.join(' ')];
+  return progressionItems;
 };
 
 const runProgression = () => {
   const tasks = [];
   let i = 0;
   while (i !== 3) {
-    const start1 = getRandomNumber(1, 10);
-    const step1 = getRandomNumber(1, 10);
-    const length1 = getRandomNumber(5, 10);
-    const question = getProgression(start1, step1, length1).toString();
-    const rightAnswer = getRandomNumber();
+    const startValue = getRandomNumber(5, 10);
+    const stepValue = getRandomNumber(5, 10);
+    const lengthValue = getRandomNumber(5, 10);
+	 const progression = getProgression(startValue, stepValue, lengthValue);
+	 const indexOfHiddenElement = getRandomNumber(0, getProgression.length);
+	 const rightAnswer = String(progression[indexOfHiddenElement]);
+	 progression[indexOfHiddenElement] = '..';
+	 const question = progression.join(' ');
     tasks.push([question, rightAnswer]);
     i += 1;
   }
