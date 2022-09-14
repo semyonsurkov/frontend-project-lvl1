c2e397113469693ad1bbe83cca561e1d95f39528
semyonsurkov <semyonsurkov@outlook.com>
2022-09-14 10:18:06 +0300

    fixing eslint errors in the utils.js file

--- src/utils.js
+++ src/utils.js
@@ -1,2 +1,3 @@
+const getRandomNumber = (min = 1, max = 10) => Math.floor(Math.random() * (max - min + 1) + min);
 
-export const getRandomNumber = (min = 1, max = 10) => Math.floor(Math.random() * (max - min + 1) + min);
+export default getRandomNumber;
