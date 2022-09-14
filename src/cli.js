2b3ff189349e4cd756b3844a4a9552a7e823d9c2
semyonsurkov <semyonsurkov@outlook.com>
2022-09-14 10:17:30 +0300

    commenting on all code

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,6 @@
-import readlineSync from 'readline-sync';
+// import readlineSync from 'readline-sync';
 
-const getUserName = () => {
-  const userName = readlineSync.question('May I have your name?: ');
-  console.log(`${'Hello'}, ${userName}${'!'}`);
-};
-
+// const getUserName = () => {
+//   const userName = readlineSync.question('May I have your name?: ');
+//   console.log(`${'Hello'}, ${userName}${'!'}`);
+// };
