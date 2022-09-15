f5612467c53dba18936838576b16ae9f0cbcb19d
semyonsurkov <semyonsurkov@outlook.com>
2022-09-15 08:43:53 +0300

    fix eslint errors in prime.js

--- src/games/prime.js
+++ src/games/prime.js
@@ -4,20 +4,20 @@ import getRandomNumber from '../utils.js';
 const description = 'Answer "yes" if given number is prime. Otherwise answer "no"';
 
 const checkPrimeNumber = (num) => {
-	for (let i = 2; i < num; i++) {
-		if (num % i === 0) {
-			return 'no';
-		}
-	}
-	return 'yes';
-}
+  for (let i = 2; i < num; i += 1) {
+    if (num % i === 0) {
+      return 'no';
+    }
+  }
+  return 'yes';
+};
 
 const runPrime = () => {
   const tasks = [];
   let i = 0;
   while (i !== 3) {
     const question = getRandomNumber(2, 10);
-	 const rightAnswer = checkPrimeNumber(question);
+    const rightAnswer = checkPrimeNumber(question);
     tasks.push([question, rightAnswer]);
     i += 1;
   }
