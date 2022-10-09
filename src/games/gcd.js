25ec7f99f9a7e48532fc78d10c8bfbc89ad9eceb
semyonsurkov <semyonsurkov@outlook.com>
2022-10-09 23:53:33 +0300

    logic change gcd.js

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -1,5 +1,5 @@
 import run from '../index.js';
-import getRandomNumber from '../utils.js';
+import { getRandomNumber } from '../utils.js';
 
 const description = 'Find the greatest common divisor of given numbers.';
 
@@ -10,15 +10,19 @@ const calculateGcd = (x, y) => {
   return calculateGcd(y, x % y);
 };
 
+const generateRound = () => {
+  const number1 = getRandomNumber();
+  const number2 = getRandomNumber();
+  const question = `${number1} ${number2}`;
+  const rightAnswer = calculateGcd(number1, number2);
+  return [question, rightAnswer.toString()];
+};
+
 const runGcd = () => {
   const tasks = [];
   let i = 0;
   while (i !== 3) {
-    const number1 = getRandomNumber();
-    const number2 = getRandomNumber();
-    const question = `${number1} ${number2}`;
-    const rightAnswer = calculateGcd(number1, number2);
-    tasks.push([question, rightAnswer.toString()]);
+    tasks.push(generateRound());
     i += 1;
   }
   run(tasks, description);
