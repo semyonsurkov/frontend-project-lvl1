a7b5dc1cefc91c1c4fd6b44d6d3bf5ea1e84fc84
semyonsurkov <semyonsurkov@outlook.com>
2022-10-09 23:22:33 +0300

    add getRandomIndex in utils.js

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,4 @@
 const getRandomNumber = (min = 1, max = 10) => Math.floor(Math.random() * (max - min + 1) + min);
+const getRandomIndex = (data) => data[Math.floor(Math.random() * data.length)];
 
-export default getRandomNumber;
+export { getRandomNumber, getRandomIndex };
