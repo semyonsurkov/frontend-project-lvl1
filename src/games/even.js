067738c8c6e9b13f9016fa8958ac76b56a41837e
semyonsurkov <semyonsurkov@outlook.com>
2022-10-09 23:53:23 +0300

    logic change even.js

--- src/games/even.js
+++ src/games/even.js
@@ -1,15 +1,19 @@
 import run from '../index.js';
-import getRandomNumber from '../utils.js';
+import { getRandomNumber } from '../utils.js';
 
 const description = 'Answer "yes" if the number is even, otherwise answer "no".';
+
+const generateRound = () => {
+  const question = getRandomNumber(1, 10);
+  const rightAnswer = (question % 2 === 0 ? 'yes' : 'no');
+  return [question, rightAnswer];
+};
 
 const runEven = () => {
   const tasks = [];
   let i = 0;
   while (i !== 3) {
-    const question = getRandomNumber(1, 10);
-    const rightAnswer = (question % 2 === 0 ? 'yes' : 'no');
-    tasks.push([question, rightAnswer]);
+    tasks.push(generateRound());
     i += 1;
   }
   run(tasks, description);
