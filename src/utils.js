994b998c561b4b0f89ceac90ca94821c6c8a862b
semyonsurkov <semyonsurkov@outlook.com>
2022-10-12 11:44:58 +0300

    update utils.js

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,4 @@
 const getRandomNumber = (min = 1, max = 10) => Math.floor(Math.random() * (max - min + 1) + min);
-const getRandomIndex = (data) => data[Math.floor(Math.random() * data.length)];
+const getRandomIndex = (data) => Math.floor(Math.random() * data.length);
 
 export { getRandomNumber, getRandomIndex };
