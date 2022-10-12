cea6d6d72665b866ef959bab709149417ffacca7
semyonsurkov <semyonsurkov@outlook.com>
2022-10-12 11:45:40 +0300

    update prime.js

--- src/games/prime.js
+++ src/games/prime.js
@@ -19,13 +19,7 @@ const generateRound = () => {
 };
 
 const runPrime = () => {
-  const tasks = [];
-  let i = 0;
-  while (i !== 3) {
-    tasks.push(generateRound());
-    i += 1;
-  }
-  run(tasks, description);
+  run(generateRound, description);
 };
 
 export default runPrime;
