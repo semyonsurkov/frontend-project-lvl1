87ad0e50e899d5bea341f032e385d4f797273255
semyonsurkov <semyonsurkov@outlook.com>
2022-10-14 00:46:56 +0300

    Update even.js

--- src/games/even.js
+++ src/games/even.js
@@ -2,11 +2,13 @@ import run from '../index.js';
 import { getRandomNumber } from '../utils.js';
 
 const description = 'Answer "yes" if the number is even, otherwise answer "no".';
+const minRange = 1;
+const maxRange = 10;
 
 const isEven = (number) => (number % 2 === 0 ? 'yes' : 'no');
 
 const generateRound = () => {
-  const question = getRandomNumber(1, 10).toString();
+  const question = getRandomNumber(minRange, maxRange).toString();
   const rightAnswer = isEven(question);
   return [question, rightAnswer];
 };
