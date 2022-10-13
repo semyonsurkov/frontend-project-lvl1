97bcd1166f86ea39ce45482a92829b9b18ef69ff
semyonsurkov <semyonsurkov@outlook.com>
2022-10-14 00:28:36 +0300

    Update prime.js

--- src/games/prime.js
+++ src/games/prime.js
@@ -2,19 +2,24 @@ import run from '../index.js';
 import { getRandomNumber } from '../utils.js';
 
 const description = 'Answer "yes" if given number is prime. Otherwise answer "no"';
+const minRange = 2;
+const maxRange = 10;
 
-const checkPrimeNumber = (number) => {
-  for (let i = 2; i < number; i += 1) {
+const isPrime = (number) => {
+  if (number < 2) {
+    return false;
+	}
+	for (let i = 2; i <= Math.sqrt(number); i += 1) {
     if (number % i === 0) {
-      return 'no';
+      return false;
     }
   }
-  return 'yes';
+  return true;
 };
 
 const generateRound = () => {
-  const question = getRandomNumber(2, 10);
-  const rightAnswer = checkPrimeNumber(question);
+  const question = getRandomNumber(minRange, maxRange);
+  const rightAnswer = isPrime(question) === true ? 'yes' : 'no';
   return [question, rightAnswer];
 };
 
