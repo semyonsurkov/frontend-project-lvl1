8637ace4d7bc0153706c1c8171a7cd22d0543c09
semyonsurkov <semyonsurkov@outlook.com>
2022-10-14 00:28:48 +0300

    Update progression.js

--- src/games/progression.js
+++ src/games/progression.js
@@ -2,6 +2,12 @@ import run from '../index.js';
 import { getRandomNumber, getRandomIndex } from '../utils.js';
 
 const description = 'What number is missing in the progression?';
+const minRange = 5;
+const maxRange = 10;
+const minStep = 1;
+const maxStep = 5;
+const minLength = 6;
+const maxLength = 10;
 
 const getProgression = (start, step, length) => {
   const progressionItems = [];
@@ -14,13 +20,13 @@ const getProgression = (start, step, length) => {
 };
 
 const generateRound = () => {
-  const startValue = getRandomNumber(5, 10);
-  const stepValue = getRandomNumber(1, 5);
-  const lengthValue = getRandomNumber(6, 10);
-  const progression = getProgression(startValue, stepValue, lengthValue);
-  const indexOfHiddenElement = getRandomIndex(getProgression);
-  const rightAnswer = progression[indexOfHiddenElement].toString();
-  progression[indexOfHiddenElement] = '..';
+  const start = getRandomNumber(minRange, maxRange);
+  const step = getRandomNumber(minStep, maxStep);
+  const length = getRandomNumber(minLength, maxLength);
+  const progression = getProgression(start, step, length);
+  const hiddenElementIndex = getRandomIndex(progression);
+  const rightAnswer = progression[hiddenElementIndex].toString();
+  progression[hiddenElementIndex] = '..';
   const question = progression.join(' ');
   return [question, rightAnswer];
 };
