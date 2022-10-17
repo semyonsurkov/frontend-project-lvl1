7e58376bdcf4fbafa8d142816c06d9f1c71a317e
semyonsurkov <semyonsurkov@outlook.com>
2022-10-17 11:19:19 +0300

    Update even.js

--- src/games/even.js
+++ src/games/even.js
@@ -8,9 +8,9 @@ const maxRange = 10;
 const isEven = (number) => number % 2 === 0;
 
 const generateRound = () => {
-  const number = getRandomNumber(minRange, maxRange)
+  const number = getRandomNumber(minRange, maxRange);
   const rightAnswer = isEven(number) ? 'yes' : 'no';
-	const question = number.toString();
+  const question = number.toString();
   return [question, rightAnswer];
 };
 
