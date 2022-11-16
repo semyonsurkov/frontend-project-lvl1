0becc53612ec295a48f1c2236a274d9ba57c3882
semyonsurkov <semyonsurkov@outlook.com>
2022-11-17 00:23:50 +0300

    Fix translate in brain-games.js

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -7,7 +7,7 @@ import runPrime from '../src/games/prime.js';
 import runProgression from '../src/games/progression.js';
 
 console.log('1.calculator\n2.parity check\n3.greatest common divisor\n4.is it prime\n5.progression');
-const userAnswer = readlineSync.question('Choose a game with a number: ');
+const userAnswer = readlineSync.question('Select a game with a help of number: ');
 if (userAnswer === '1') {
 	runCalc();
 }
