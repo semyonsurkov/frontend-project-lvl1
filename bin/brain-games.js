afe09610bc8f53942102beed1c0b1d087a2e6211
semyonsurkov <semyonsurkov@outlook.com>
2022-11-17 11:21:50 +0300

    Update brain-games.js

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,25 +1,5 @@
-#!/usr/bin/env node
-import readlineSync from 'readline-sync';
-import runCalc from '../src/games/calc.js';
-import runEven from '../src/games/even.js';
-import runGcd from '../src/games/gcd.js';
-import runPrime from '../src/games/prime.js';
-import runProgression from '../src/games/progression.js';
+import getUserName from '../src/cli.js';
 
-console.log('1.calculator\n2.parity check\n3.greatest common divisor\n4.is it prime\n5.progression');
-const userAnswer = readlineSync.question('Select a game with a help of number: ');
-if (userAnswer === '1') {
-	runCalc();
-}
-if (userAnswer === '2') {
-	runEven();
-}
-if (userAnswer === '3') {
-	runGcd();
-}
-if (userAnswer === '4') {
-	runPrime();
-}
-if (userAnswer === '5') {
-	runProgression();
-}
+console.log('Welcome to the Brain Games!');
+getUserName();
+
